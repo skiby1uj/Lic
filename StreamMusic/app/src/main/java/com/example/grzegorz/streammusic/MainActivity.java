ebd255ba98e2c7aece2a632e952eab63340bf27e
grzegorz <grzegorz.krzeszowski@student.uj.edu.pl>
2017-04-20 23:42:58 +0200

    dodanie dialogu wyboru czy ma wysylac czy odbierac

--- StreamMusic/app/src/main/java/com/example/grzegorz/streammusic/MainActivity.java
+++ StreamMusic/app/src/main/java/com/example/grzegorz/streammusic/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.grzegorz.streammusic;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -17,18 +21,49 @@ public class MainActivity extends Activity {
 
     RadioButton chWysylaj = null;
     RadioButton chOdbieraj = null;
+    Button bPlay = null;
+    boolean isServer = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.onCreateDialog(savedInstanceState, this).show();
+        Log.e("INFO", "What is this: " + isServer);
         this.runBluetooth();
         chWysylaj = (RadioButton)findViewById(R.id.chWysylaj);
         chOdbieraj = (RadioButton)findViewById(R.id.chOdbieraj);
+        bPlay = (Button) findViewById(R.id.button);
 
 //        dajSieWykryc();
 //        wykryjInne();
+    }
+
+    public Dialog onCreateDialog(Bundle savedInstanceState, final Context context) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setMessage("W jaki sposób ma działać urządzenie?")
+                .setPositiveButton("Wysyłać", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                        isServer = true;
+                        chWysylaj.setChecked(true);
+                        chWysylaj.setEnabled(false);
+                        chOdbieraj.setEnabled(false);
+                    }
+                })
+                .setNegativeButton("Odbierać", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        isServer = false;
+                        chOdbieraj.setChecked(true);
+                        chOdbieraj.setEnabled(false);
+                        chWysylaj.setEnabled(false);
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
 
     void runBluetooth(){
@@ -43,12 +78,11 @@ public class MainActivity extends Activity {
         if (chWysylaj.isChecked()){
             Thread t = new ServerBluetooth(this);
             t.start();
-//            new ServerBluetooth(this).run();
         }
         else if (chOdbieraj.isChecked()){
-            Thread t = new ClientBluetooth("60:A4:4C:C3:2C:6A", this);
-            t.start();
-//            new ClientBluetooth("60:A4:4C:C3:2C:6A", this).run();//NEXUS 60:A4:4C:C3:2C:6A  SONY BC:6E:64:B5:C1:45  LG 98:D6:F7:C9:98:45
+            new ClientBluetooth("60:A4:4C:C3:2C:6A", this);
+//            t.start();
+            //NEXUS 60:A4:4C:C3:2C:6A  SONY BC:6E:64:B5:C1:45  LG 98:D6:F7:C9:98:45
         }
         else {
             Toast.makeText(this, "Nie wybrano funkcjonalnosci urzadzenia", Toast.LENGTH_LONG).show();
